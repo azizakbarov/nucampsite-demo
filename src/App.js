c996832a90b87052f7ce264febab838984308239
AzizAkbarov <azizbakbarov@gmail.com>
2024-05-22 14:35:16 +0500

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import {Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePages';
 import './App.css';
 import Header from './components/Header';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Footer from './components/Footer';
 
+
 function App() {
     return (
         <div className='App'>
             <Header/>
-            <CampsitesDirectoryPage />
+            <Routes>
+                <Route path='/' element={<HomePage/>} />
+                <Route path='contact' element={<ContactPage/>}/>
+                <Route path='directory' element={ < CampsitesDirectoryPage/>} />
+            </Routes>
             <Footer />
         </div>
     );
